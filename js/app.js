f588ec2242822f59b33638ed6e0059109c073276
ArisPant <aris-pant@hotmail.com>
2018-05-07 22:17:18 +0300

    Player's class skeleton have been set and some object instatiations to make the game functional

--- js/app.js
+++ js/app.js
@@ -24,13 +24,34 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
 
+    constructor(name){
+        this.name = name;
+    }
+
+    update(){
+
+    }
+
+    render(){
+
+    }
+
+    handleInput(){
+
+    }
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let enemy1 = new Enemy();
+let enemy2 = new Enemy();
 
-
+let allEnemies = [enemy1, enemy2];
+var player = new Player('aris');
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
